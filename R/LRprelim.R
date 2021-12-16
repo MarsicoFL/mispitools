c6a172e8b70191c6642757ecda5b59112fd0dbdd
Franco Marsico <franco.lmarsico@gmail.com>
2021-12-16 09:11:41 -0300

    Add files via upload

--- R/LRprelim.R
+++ R/LRprelim.R
@@ -8,33 +8,34 @@
 #' @param type Type of scenario, type 1 is an "open search", where it is unknown if the missing person is in the database. Type 2 refers to a scenario where the missing person is in the database.
 #' @param PrelimData Used when type = 2, is the dataframe with the DBD of the persons of interest in the database.
 #' @param Draw Number of simulations for Dirichlet distribution computation.
+#' @param seed Seed for simulations.
 #' @import dirichlet
 #' @export
 #' @return A value of Likelihood ratio based on preliminary investigation data. In this case, birth date.
 #' @examples
 #' library(dirichlet)
-#' LRprelim(ABD = "1976-05-31", DBD = "1976-07-15", PrelimData, alpha = c(1, 4, 60, 11, 6, 4, 4), cuts = c(-120, -30, 30, 120, 240, 360), type = 1)
+#' LRprelim(ABD = "1976-05-31", DBD = "1976-07-15", PrelimData, alpha = c(1, 4, 60, 11, 6, 4, 4), cuts = c(-120, -30, 30, 120, 240, 360), type = 1, seed = 123)
 
 
 
-LRprelim = function(ABD = "1976-05-31", DBD = "1976-07-15", PrelimData, alpha = c(1, 4, 60, 11, 6, 4, 4), cuts = c(-120, -30, 30, 120, 240, 360), draw = 500, type = 1) {
-
+LRprelim = function(ABD = "1976-05-31", DBD = "1976-07-15", PrelimData, alpha = c(1, 4, 60, 11, 6, 4, 4), cuts = c(-120, -30, 30, 120, 240, 360), draw = 500, type = 1, seed = 123) {
+seed = seed
 x = dirichlet::rdirichlet(draw, alpha)
-fit = data.frame(dirichlet::fit.genDirichlet(x, "ml"))
+fit = data.frame(dirichlet::fit.dirichlet(x, "mm"))
 fit2 = as.list(fit$p)
 DBD = as.Date(DBD)
 ABD = as.Date(ABD)
 
 Dis0 = julian(DBD,ABD)
-Dis = Dis0[1]
+Dist = Dis0[1]
 i = 0   
 w = length(alpha) - 1
-if(Dis <= cuts[1]) {H1 = fit2[1]} 
-if(Dis >= cuts[w]) {H1 = fit2[w]}
-if(Dis >= cuts[1] & Dis <= cuts[w]) {
+if(Dist <= cuts[1]) {H1 = fit2[1]} 
+if(Dist >= cuts[w]) {H1 = fit2[w]}
+if(Dist >= cuts[1] & Dist <= cuts[w]) {
 for (i in 1:w) {
     up = i + 1
-    if (Dis > cuts[i]  & Dis < cuts[up]){H1 = fit2[i]}}}
+    if (Dist > cuts[i]  & Dist < cuts[up]){H1 = fit2[i]}}}
                 
 if(type == 1) {
   H2 = 1/length(alpha)
@@ -46,14 +47,14 @@ if(type == 2) {
   PrelimData <- as.data.frame(table(cut(PrelimData$Dis, breaks = c(-Inf,cuts, Inf))))
   alpha2 <- as.vector(PrelimData$Freq)
   x2 = dirichlet::rdirichlet(draw, alpha2)
-  fit3 = data.frame(fit.genDirichlet(x2, "ml"))
-  
-  if(Dis <= cuts[1]){H2 = fit3[1]}
-  if(Dis >= cuts[w]){H2 = fit3[w]}
-  if(Dis >= cuts[1] & Dis <= fit3[w]) {
+  fit3_0 = data.frame(fit.dirichlet(x2, "mm"))
+  fit3 = as.list(fit3_0$p)
+  if(Dist <= cuts[1]){H2 = fit3[1]}
+  if(Dist >= cuts[w]){H2 = fit3[w]}
+  if(Dist >= cuts[1] & Dist <= cuts[w]) {
   for (i in 1:w) {
   up = i + 1
-  if (Dis > cuts[i]  & Dis < cuts[up]){H2 = fit3[i]}}}
+  if (Dist > cuts[i]  & Dist < cuts[up]){H2 = fit3[i]}}}
 
   LR = as.numeric(H1)/as.numeric(H2)
 }
