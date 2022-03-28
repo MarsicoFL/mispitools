69f1d7ab8a6e713804a061e60798e903a0323caf
Franco Marsico <franco.lmarsico@gmail.com>
2022-03-28 07:57:14 -0300

    Add files via upload

---
+++ Asia.R
@@ -0,0 +1,6 @@
+#' STRs allelic frequencies from specified country.
+#'
+#' A dataset of allele frequencies.
+#'
+#' @format A data frame allele frequencies
+"Asia"
---
+++ BosniaHerz.R
@@ -0,0 +1,5 @@
+#' STRs allelic frequencies from specified country.
+#'
+#'
+#' @format A data frame allele frequencies
+"BosniaHerz"
