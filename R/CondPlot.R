de0b514c6fbca9014b9d79456c0287ac0846b4de
Franco Marsico <franco.lmarsico@gmail.com>
2023-04-25 12:49:44 -0300

    Update CondPlot.R

--- R/CondPlot.R
+++ R/CondPlot.R
@@ -23,7 +23,9 @@ p1 <- ggplot2::ggplot(POP, aes(x = Var2, y = Var1)) +
   labs(x="Hair colour (C)", y="Biologocial sex-Age", title="P(D|H2)", limits = c(0,1)) +
   theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                      axis.text.y=element_text(size=13),
-                     plot.title=element_text(size=13))
+                     plot.title=element_text(size=13)) + 
+   geom_text(aes(label = as.character(format(round(value, 2), nsmall = 2))))
+
 
 MP <- reshape2::melt(CPT_MP)
 p2 <- ggplot2::ggplot(MP, aes(x = Var2, y = Var1)) +
@@ -32,7 +34,9 @@ p2 <- ggplot2::ggplot(MP, aes(x = Var2, y = Var1)) +
   labs(x="Hair colour (C)", y="Biologocial sex-Age", title="P(D|H1)") +
   theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                      axis.text.y=element_text(size=13),
-                     plot.title=element_text(size=13))
+                     plot.title=element_text(size=13)) + 
+  geom_text(aes(label = as.character(format(round(value, 2), nsmall = 2))))
+
 LRtable <- log10(CPT_MP/CPT_POP)
 LR <- reshape2::melt(LRtable)
 p3 <- ggplot2::ggplot(LR, aes(x = Var2, y = Var1)) +
@@ -41,7 +45,8 @@ p3 <- ggplot2::ggplot(LR, aes(x = Var2, y = Var1)) +
   labs(x="Hair colour (C)", y="Biologocial sex-Age", title="Log10(LR)") +
   theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                      axis.text.y=element_text(size=13),
-                     plot.title=element_text(size=13))
+                     plot.title=element_text(size=13)) + 
+ geom_text(aes(label = as.character(format(round(value, 2), nsmall = 2))))
 p <- ((p1+p2+p3)) + patchwork::plot_annotation(tag_levels = 'A')
 p
 return(p)}
