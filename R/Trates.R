d71e187a02f9e9f146dbe1cc0b657956bc0d222e
Franco Marsico <franco.lmarsico@gmail.com>
2024-08-01 17:43:21 -0300

    Update Trates.R

--- R/Trates.R
+++ R/Trates.R
@@ -18,21 +18,9 @@
 
 Trates = function(datasim, threshold) {
 
-unrelated_values <- vector()
-related_values <- vector()
-
-list_length <- length(datasim[["Unrelated"]])
-
-for (i in 1:list_length) {
-  unrelated_value <- datasim[["Unrelated"]][[i]][["LRtotal"]][["H1:H2"]]
-  related_value <- datasim[["Related"]][[i]][["LRtotal"]][["H1:H2"]]
-  
-  unrelated_values <- c(unrelated_values, unrelated_value)
-  related_values <- c(related_values, related_value)
-}
-
-results_df <- data.frame(Unrelated = unrelated_values, Related = related_values)
-datasim <- results_df
+if (!is.data.frame(datasim)) {
+   datasim <- simLR2dataframe(datasim)
+ }
 
 nsims = nrow(datasim)
 TPED = datasim$Related 
