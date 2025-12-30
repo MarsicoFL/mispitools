e9bdbaef93b359df6d0e61c62cb1295f4c90a2b3
Franco Marsico <franco.lmarsico@gmail.com>
2025-12-29 22:17:45 -0300

    Fix biological label spelling in plots

--- R/mispiApp.R
+++ R/mispiApp.R
@@ -98,7 +98,7 @@ server <- function(input, output) {
     p1 <- ggplot2::ggplot(POP, aes(x = Var2, y = Var1)) +
       ggplot2::geom_raster(aes(fill=value)) +
       scale_fill_gradient(low="grey90", high="blue", limits = c(0,1)) +
-      labs(x="Hair colour (C)", y="Biologocial sex-Age", title="P(D|H2)", limits = c(0,1)) +
+      labs(x="Hair colour (C)", y="Biological sex-Age", title="P(D|H2)", limits = c(0,1)) +
       theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                          axis.text.y=element_text(size=13),
                          plot.title=element_text(size=13)) +
@@ -107,7 +107,7 @@ server <- function(input, output) {
     p2 <- ggplot2::ggplot(MP, aes(x = Var2, y = Var1)) +
       ggplot2::geom_raster(aes(fill=value)) +
       scale_fill_gradient(low="grey90", high="blue", limits = c(0,1)) +
-      labs(x="Hair colour (C)", y="Biologocial sex-Age", title="P(D|H1)") +
+      labs(x="Hair colour (C)", y="Biological sex-Age", title="P(D|H1)") +
       theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                          axis.text.y=element_text(size=13),
                          plot.title=element_text(size=13)) +
@@ -117,7 +117,7 @@ server <- function(input, output) {
     p3 <- ggplot2::ggplot(LR, aes(x = Var2, y = Var1)) +
       ggplot2::geom_raster(aes(fill=value)) +
       scale_fill_gradient(low="grey90", high="blue") +
-      labs(x="Hair colour (C)", y="Biologocial sex-Age", title="Log10(LR)") +
+      labs(x="Hair colour (C)", y="Biological sex-Age", title="Log10(LR)") +
       theme_bw() + theme(axis.text.x=element_text(size=13, angle=0, vjust=0.3),
                          axis.text.y=element_text(size=13),
                          plot.title=element_text(size=13)) +
